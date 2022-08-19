91697a941041c792ba23e709ad5fe2a65cf117ef
RoseMartin15 <111441859+RoseMartin15@users.noreply.github.com>
2022-08-18 23:24:51 -0400

    Add files via upload

---
+++ finalproject/finalproject/CodeQuizViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CodeQuizViewController.swift
+//  finalproject
+//
+//  Created by student on 8/18/22.
+//
+
+import UIKit
+
+class CodeQuizViewController: UIViewController {
+
+    @IBOutlet weak var yesText: UITextView!
+    
+    @IBAction func yesClick(_ sender: UIButton) {
+        
+        yesText.text = "yes response"
+    }
+    
+    
+    override func viewDidLoad() {
+    //    super.viewDidLoad()
+// Do any additional setup after loading the view.
+   }
+}
+
+
+    
+
+    
+    
+    
+    
+    
+    
---
+++ finalproject/finalproject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  finalproject
+//
+//  Created by İpek Altunyurt on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+  
+
